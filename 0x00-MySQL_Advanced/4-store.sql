f131d0e46134c4f7279e7aa5dcdf8d1450f3c337
dtemilade <dtemilade@gmail.com>
2024-04-27 10:17:39 +0100

    Updating commits for task accomplished

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,6 @@ DELIMITER //
 CREATE TRIGGER decrease_quantity_trigger AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    -- Decrease the quantity of the item in the items table
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
