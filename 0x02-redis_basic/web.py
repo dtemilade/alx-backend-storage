5ff5760a192bfbf87ea644fe8b6426528601da45
dtemilade <dtemilade@gmail.com>
2024-05-03 12:05:03 +0100

    Updating commits for task accomplished

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,32 +8,42 @@ store = redis.Redis()
 
 
 def count_url_access(method):
-    """Track how many times a particular URL was accessed in the key"""
+    """Decorator to track how many times a particular URL was accessed"""
     @wraps(method)
     def wrapper(url):
-        count_key = "count:" + url
         cached_key = "cached:" + url
-
-        # Increment the count of URL access
-        store.incr(count_key)
-
-        # Get the cached HTML if exists
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
-        # Fetch HTML content from URL
+        count_key = "count:" + url
         html = method(url)
 
-        # Cache the HTML with an expiration time of 10 seconds
-        store.setex(cached_key, 10, html)
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
+
+def cache_with_expiration(method):
+    """Decorator to cache the result with an expiration time of 10 seconds"""
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        html = method(url)
+        store.setex(cached_key, 10, html)
         return html
     return wrapper
 
 
 @count_url_access
+@cache_with_expiration
 def get_page(url: str) -> str:
-    """Return the obtained HTML content of a particular URL"""
+    """Obtain the HTML content of a particular URL"""
     retval = requests.get(url)
     return retval.text
