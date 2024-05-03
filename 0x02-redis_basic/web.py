6b3ed42ccafb51cf53e74f00757256c650badd2b
dtemilade <dtemilade@gmail.com>
2024-05-03 11:58:59 +0100

    Updating commits for task accomplished

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,26 +8,33 @@ store = redis.Redis()
 
 
 def count_url_access(method):
-    """  track how many times a particular URL was accessed in the key """
+    """Track how many times a particular URL was accessed in the key"""
     @wraps(method)
+
     def wrapper(url):
+        count_key = "count:" + url
         cached_key = "cached:" + url
+
+        # Increment the count of URL access
+        store.incr(count_key)
+
+        # Get the cached HTML if exists
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
-        count_key = "count:" + url
+        # Fetch HTML content from URL
         html = method(url)
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        # Cache the HTML with an expiration time of 10 seconds
+        store.setex(cached_key, 10, html)
+
         return html
     return wrapper
 
 
 @count_url_access
 def get_page(url: str) -> str:
-    """ Return the obtained the HTML content of a particular URL """
+    """Return the obtained HTML content of a particular URL"""
     retval = requests.get(url)
     return retval.text
